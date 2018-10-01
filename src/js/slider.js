3c7c71d76fea613b568fdbbd743aba5f75c63075
delpher.pl <delpher00@wp.pl>
2018-10-01 22:53:10 +0200

    UPD js - SLIDER dispatch own event & some fixes

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,35 @@
-function showslide(pageNumber, sliderEl) {
-  sliderEl.setAttribute(
+
+//
+// sliderEl argument must have data-currentslide attribute!
+//
+
+
+function getCurrentSlide(sliderEl) {
+  const slidesCount = sliderEl.children.length;
+  const currentSlide = +sliderEl.dataset.currentslide;
+  return currentSlide % slidesCount;
+}
+
+function showslide(pageNumber, sliderEl, isDispatchingEv = false) {
+  const SLIDER_EL = sliderEl;
+  SLIDER_EL.dataset.currentslide = pageNumber;
+
+  SLIDER_EL.setAttribute(
     'style',
     `transform:translateX(-${100 * pageNumber}%)`,
   );
+
+  if (isDispatchingEv) {
+    SLIDER_EL.dispatchEvent(new Event('changeSlide'));
+  }
 }
 
-function moveSlide(moveSteps, sliderEl) {
+function moveSlide(moveSteps, sliderEl, isDispatchingEv = false) {
   const SLIDER_EL = sliderEl;
-
   const slidesCount = SLIDER_EL.children.length;
   let currentSlide = +SLIDER_EL.dataset.currentslide;
   currentSlide += moveSteps;
+
   if (currentSlide < 0) {
     currentSlide = slidesCount - 1;
   }
@@ -20,9 +39,13 @@ function moveSlide(moveSteps, sliderEl) {
     'style',
     `transform:translateX(-${(100 * currentSlide) % (slidesCount * 100)}%)`,
   );
+  if (isDispatchingEv) {
+    SLIDER_EL.dispatchEvent(new Event('changeSlide'));
+  }
 }
 
 module.exports = {
+  getCurrentSlide,
   showslide,
   moveSlide,
 };
