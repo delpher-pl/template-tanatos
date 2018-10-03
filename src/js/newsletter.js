d0677d3d7c531c45b84c0e38dacceb016d2dbf9d
delpher.pl <delpher00@wp.pl>
2018-10-04 00:17:43 +0200

    UPD js - NEWSLETTER add email validation

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -1,5 +1,24 @@
+const { isValidEmail } = require('./validate.js');
+
+const newsletterInput = document.querySelector('#newsletter-email');
 const newsletterBtn = document.querySelector('#newsletter-submit');
+
+newsletterInput.addEventListener('change', (ev) => {
+  const INPUT_EL = ev.target;
+  INPUT_EL.value = INPUT_EL.value.toLowerCase();
+
+  if (isValidEmail(INPUT_EL.value)) {
+    newsletterBtn.classList.remove('btn--error');
+  }
+}, false);
 
 newsletterBtn.addEventListener('click', (ev) => {
   ev.preventDefault();
-});
+
+  if (isValidEmail(newsletterInput.value)) {
+    console.log('Valid email! Can send it to server.');
+    ev.target.classList.remove('btn--error');
+  } else {
+    ev.target.classList.add('btn--error');
+  }
+}, false);
