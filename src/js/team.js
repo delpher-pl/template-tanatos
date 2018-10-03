b481057a7c80fd7e115e46f131737ea7b74653f0
delpher.pl <delpher00@wp.pl>
2018-10-03 14:30:28 +0200

    UPD js - TEAM add swipe.js & pagination update

--- src/js/team.js
+++ src/js/team.js
@@ -1,10 +1,29 @@
-const { showslide } = require('./slider.js');
+const { showslide, getCurrentSlide } = require('./slider.js');
+const { Swipe } = require('./swipe.js');
 
+const teamSection = document.querySelector('.page__team');
 const teamSlider = document.querySelector('.team__slider');
 const teamPagination = document.querySelector('#team-pagination');
 let selectedSlide = 0;
 
+function updatePagination(sliderEl) {
+  const currentSlide = getCurrentSlide(sliderEl);
+  teamPagination.querySelectorAll('label')[currentSlide].click();
+}
+
+teamSlider.addEventListener('changeSlide', () => {
+  updatePagination(teamSlider);
+});
+
 teamPagination.addEventListener('change', (ev) => {
   selectedSlide = ev.target.value;
+
   showslide(selectedSlide, teamSlider);
 }, false);
+
+
+const swipe = new Swipe({
+  detectEl: teamSection,
+  moveEl: teamSlider,
+});
+swipe.init();
