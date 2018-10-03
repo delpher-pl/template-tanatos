59665f8a28a58a40d244fcde7a51ea3f293b4636
delpher.pl <delpher00@wp.pl>
2018-10-03 23:45:21 +0200

    ADD js - VALIDATE

---
+++ src/js/validate.js
@@ -0,0 +1,9 @@
+function isValidEmail(email) {
+  const re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
+  return re.test(email);
+}
+
+
+module.exports = {
+  isValidEmail,
+};
