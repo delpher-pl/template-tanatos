85f4e2f687cf4d75e3e89a60bb7f27d487717130
delpher.pl <delpher00@wp.pl>
2018-10-03 15:18:04 +0200

    UPD js - TESTIMONIALS add swipe.js

--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -1,8 +1,14 @@
 const { moveSlide } = require('./slider.js');
+const { Swipe } = require('./swipe.js');
 
+const testimonialsSection = document.querySelector('.page__testimonials');
 const testimonialsSlider = document.querySelector('.testimonials__list');
 const testimonialsPrev = document.querySelector('#testimonials-prev');
 const testimonialsNext = document.querySelector('#testimonials-next');
+
+testimonialsSlider.addEventListener('changeSlide', () => {
+  console.log('changeSlide detected in testimonials!');
+});
 
 testimonialsPrev.addEventListener('click', (ev) => {
   ev.preventDefault();
@@ -13,3 +19,10 @@ testimonialsNext.addEventListener('click', (ev) => {
   ev.preventDefault();
   moveSlide(1, testimonialsSlider);
 }, false);
+
+
+const swipe = new Swipe({
+  detectEl: testimonialsSection,
+  moveEl: testimonialsSlider,
+});
+swipe.init();
