37e55e51af2d834a6eed0d55d56b69433c06c831
delpher.pl <delpher00@wp.pl>
2018-10-11 19:15:45 +0200

    UPD js - VALIDATE add Validate class

--- src/js/validate.js
+++ src/js/validate.js
@@ -4,6 +4,75 @@ function isValidEmail(email) {
 }
 
 
+class Validate {
+  /**
+   * @param {Array} rulesArr - Array of arrays with two values:
+   * 1) Single DOM element like <input> or <textarea> to validate
+   * 2) RegExp to test this element
+   */
+  constructor(rulesArr, submitEl) {
+    this.rulesArr = rulesArr;
+    this.isAllRight = false;
+    this.submitEl = submitEl;
+  }
+
+  init() {
+    this._addEventListeners();
+  }
+
+  _addEventListeners() {
+    this.rulesArr.forEach((pairArr) => {
+      pairArr[0].addEventListener('change', (ev) => {
+        const EL = pairArr[0];
+        const RE = pairArr[1];
+
+        EL.value = ev.target.value.trim();
+        if (RE.test(ev.target.value)) {
+          EL.classList.remove('input-err');
+          EL.classList.add('input-ok');
+        } else {
+          EL.classList.add('input-err');
+          EL.classList.remove('input-ok');
+          this.isAllRight = false;
+        }
+        this._checkAllRules();
+      }, false);
+    });
+
+    this.submitEl.addEventListener('click', (ev) => {
+      ev.preventDefault();
+      if (this.isAllRight) {
+        this._submitErrorRemove();
+      } else {
+        this._submitErrorAdd();
+      }
+    });
+  }
+
+  _checkAllRules() {
+    const RESULT = this.rulesArr.every(pairArr => pairArr[1].test(pairArr[0].value));
+    this.isAllRight = RESULT;
+    if (this.isAllRight) {
+      this._submitErrorRemove();
+    }
+    return RESULT;
+  }
+
+  isFormValid() {
+    return this.isAllRight;
+  }
+
+  _submitErrorRemove() {
+    this.submitEl.classList.remove('btn--error');
+  }
+
+  _submitErrorAdd() {
+    this.submitEl.classList.add('btn--error');
+  }
+}
+
+
 module.exports = {
   isValidEmail,
+  Validate,
 };
