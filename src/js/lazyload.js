3b61b05c658953ffb84346cd35cef46e9e83109c
delpher.pl <delpher00@wp.pl>
2019-01-08 19:30:57 +0100

    UPD js - LAZYLOAD add lazyUpdTimeout() & module.exports

--- src/js/lazyload.js
+++ src/js/lazyload.js
@@ -5,3 +5,14 @@ const lazyLoad = new LazyLoad({
   elements_selector: '.lazy',
 });
 /* eslint-enable no-unused-vars */
+
+function lazyUpdTimeout() {
+  setTimeout(() => {
+    lazyLoad.update();
+  }, 350);
+}
+
+module.exports = {
+  lazyLoad,
+  lazyUpdTimeout,
+};
